0a092b666a212f6655c125ac80fe3d0311eedc8c
Jhimy Rua <ruaechalarjhimyaldair63@gmail.com>
2026-04-03 17:24:36 -0400

    lab1

---
+++ lab1NavesUSFX_0012026/Source/NavesUSFX_0012026.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class NavesUSFX_0012026Target : TargetRules
+{
+	public NavesUSFX_0012026Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("NavesUSFX_0012026");
+	}
+}
